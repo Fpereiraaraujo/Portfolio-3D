f6795c451f1e1cecd2761f98efcbfc427136f509
Fernando pereira <fernandomendespereira@hotmail.com>
2023-11-13 11:12:54 -0300

    finding assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
+import { Footer, Navbar } from "./components";
+import { About, Contact, Home, Projects } from "./pages";
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <main className='bg-slate-300/20'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route
+            path='/*'
+            element={
+              <>
+                <Routes>
+                  <Route path='/about' element={<About />} />
+                  <Route path='/projects' element={<Projects />} />
+                  <Route path='/contact' element={<Contact />} />
+                </Routes>
+                <Footer />
+              </>
+            }
+          />
+        </Routes>
+      </Router>
+    </main>
+  );
+};
 
-export default App
+export default App;
