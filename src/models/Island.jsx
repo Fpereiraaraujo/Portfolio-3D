6dff06e7cadbd1bf8630503b424beac2a3ac6c32
Fernando pereira <fernandomendespereira@hotmail.com>
2023-11-13 17:07:19 -0300

    nomalize rotation

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -17,7 +17,7 @@ import { a } from "@react-spring/three";
 
 import islandScene from "../assets/3d/island.glb";
 import { useEffect, useRef } from "react";
-import { useThree } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
 
 const Island = ({ isRotating, setIsRotating, ...props }) => {
   const islandRef = useRef();
@@ -31,45 +31,45 @@ const Island = ({ isRotating, setIsRotating, ...props }) => {
 
   
 
-  const handlePointerDown = (e) => {
-    e.stopProgation();
-    e.preventDefault();
+   const handlePointerDown = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
     setIsRotating(true);
 
-
-    const clientX = e.touches
-      ? e.touches[0].clientX
-      : e.clientX
+    const clientX = event.touches ? event.touches[0].clientX : event.clientX;
 
     lastX.current = clientX;
   }
 
-  const handlePointerUp = (e) => {
-    e.stopProgation();
-    e.preventDefault();
+  const handlePointerUp = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
     setIsRotating(false);
+  };
 
-    const clientX = e.touches
-      ? e.touches[0].clientX
-      : e.clientX
-
-    const delta = (clientX - lastX.current) / viewport.width;
-
-
-    islandRef.current.rotation.y += delta * 0.01 * Math.PI;
-    lastX.current = clientX;
-    rotationSpeed.current = delta * 0.01 * Math.PI
-
-  }
-  const handlePointerMove = (e) => {
-    e.stopProgation();
-    e.preventDefault();
-
+   const handlePointerMove = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
     if (isRotating) {
-      handlerPointerUp(e)
-    }
-  }
+      // If rotation is enabled, calculate the change in clientX position
+      const clientX = event.touches ? event.touches[0].clientX : event.clientX;
 
+      // calculate the change in the horizontal position of the mouse cursor or touch input,
+      // relative to the viewport's width
+      const delta = (clientX - lastX.current) / viewport.width;
+
+      // Update the island's rotation based on the mouse/touch movement
+      islandRef.current.rotation.y += delta * 0.01 * Math.PI;
+
+      // Update the reference for the last clientX position
+      lastX.current = clientX;
+
+      // Update the rotation speed
+      rotationSpeed.current = delta * 0.01 * Math.PI;
+    }
+  };
+
+  // Handle keydown events
   const handleKeyDown = (event) => {
     if (event.key === "ArrowLeft") {
       if (!isRotating) setIsRotating(true);
@@ -84,11 +84,12 @@ const Island = ({ isRotating, setIsRotating, ...props }) => {
     }
   };
 
-    const handleKeyUp = (event) => {
-      if (event.key === "ArrowLeft" || event.key === "ArrowRight") {
-        setIsRotating(false);
-      }
-    };
+    // Handle keyup events
+  const handleKeyUp = (event) => {
+    if (event.key === "ArrowLeft" || event.key === "ArrowRight") {
+      setIsRotating(false);
+    }
+  };
 
   useEffect(() => {
     // Add event listeners for pointer and keyboard events
@@ -108,6 +109,64 @@ const Island = ({ isRotating, setIsRotating, ...props }) => {
       window.removeEventListener("keyup", handleKeyUp);
     };
   }, [gl, handlePointerDown, handlePointerUp, handlePointerMove]);
+
+
+
+  // This function is called on each frame update
+  useFrame(() => {
+    // If not rotating, apply damping to slow down the rotation (smoothly)
+    if (!isRotating) {
+      // Apply damping factor
+      rotationSpeed.current *= dampingFactor;
+
+      // Stop rotation when speed is very small
+      if (Math.abs(rotationSpeed.current) < 0.001) {
+        rotationSpeed.current = 0;
+      }
+
+      islandRef.current.rotation.y += rotationSpeed.current;
+    } else {
+      // When rotating, determine the current stage based on island's orientation
+      const rotation = islandRef.current.rotation.y;
+
+      /**
+       * Normalize the rotation value to ensure it stays within the range [0, 2 * Math.PI].
+       * The goal is to ensure that the rotation value remains within a specific range to
+       * prevent potential issues with very large or negative rotation values.
+       *  Here's a step-by-step explanation of what this code does:
+       *  1. rotation % (2 * Math.PI) calculates the remainder of the rotation value when divided
+       *     by 2 * Math.PI. This essentially wraps the rotation value around once it reaches a
+       *     full circle (360 degrees) so that it stays within the range of 0 to 2 * Math.PI.
+       *  2. (rotation % (2 * Math.PI)) + 2 * Math.PI adds 2 * Math.PI to the result from step 1.
+       *     This is done to ensure that the value remains positive and within the range of
+       *     0 to 2 * Math.PI even if it was negative after the modulo operation in step 1.
+       *  3. Finally, ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI) applies another
+       *     modulo operation to the value obtained in step 2. This step guarantees that the value
+       *     always stays within the range of 0 to 2 * Math.PI, which is equivalent to a full
+       *     circle in radians.
+       */
+      const normalizedRotation =
+        ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
+
+      
+      switch (true) {
+        case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
+          setCurrentStage(4);
+          break;
+        case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
+          setCurrentStage(3);
+          break;
+        case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
+          setCurrentStage(2);
+          break;
+        case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
+          setCurrentStage(1);
+          break;
+        default:
+          setCurrentStage(null);
+      }
+    }
+  });
 
   return (
     // {Island 3D model from: https://sketchfab.com/3d-models/foxs-islands-163b68e09fcc47618450150be7785907}
